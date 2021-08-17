b731a9e96d577e36931bf99e86b63dd638b32b6f
Sanket Valani <sanketvalani@gmail.com>
2021-08-17 16:51:18 +0530

    Added View and Model

---
+++ Fizzbuzz.MVC/Models/FizzbuzzModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PagedList;
+
+namespace Fizzbuzz.MVC.Models
+{
+    public class FizzbuzzModel
+    {
+        public string InputString { get; set; }
+        public PagedList<string> OutputList { get; set; }
+    }
+}
